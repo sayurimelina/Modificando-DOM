3cbabe0bbf622678098d6f36027265363acf44e8
Sayuri Melina <cernasayuri@gmail.com>
2023-03-14 04:41:07 -0500

    segunda clase listener

--- 1821-manipulando-dom-con-js-proyecto_inicial/main.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/main.js
@@ -6,3 +6,10 @@ document.querySelectorAll(“selector”)
 
 document.querySelector('span').textContent = 'Realizar curso de JavaScript'
 */
+
+const btn = document.querySelector("[data-form-btn]"); //seleccionamos el boton agregar usando sata atribute
+
+//listener de tipo click listener tien 2 parametros accion, lo que hace
+
+btn.addEventListener("click", function(){});
+console.log("crear tarea");

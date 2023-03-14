cae429d7f5362508ee654862472d3750311fe87d
Sayuri Melina <cernasayuri@gmail.com>
2023-03-14 04:51:06 -0500

    aula 3 - listener e input

--- 1821-manipulando-dom-con-js-proyecto_inicial/main.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/main.js
@@ -8,8 +8,15 @@ document.querySelector('span').textContent = 'Realizar curso de JavaScript'
 */
 
 const btn = document.querySelector("[data-form-btn]"); //seleccionamos el boton agregar usando sata atribute
+const input = document.querySelector("[data-form-input]");
+
+console.log(btn);
+
 
 //listener de tipo click listener tien 2 parametros accion, lo que hace
 
-btn.addEventListener("click", function(){});
-console.log("crear tarea");
+btn.addEventListener("click", function(){
+    console.log("crear tarea");
+    console.log(input.value);
+});
+

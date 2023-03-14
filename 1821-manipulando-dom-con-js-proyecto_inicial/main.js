9315e5290bd35be05517fc391f720158d27ef5cc
Sayuri Melina <cernasayuri@gmail.com>
2023-03-14 04:12:05 -0500

    Clase 01 completada

---
+++ 1821-manipulando-dom-con-js-proyecto_inicial/main.js
@@ -0,0 +1,8 @@
+/*
+document.getElementById(‘id’);
+document.getElementsByClassName(‘clase’)
+document.getElementsByTagName(‘tag’)
+document.querySelectorAll(“selector”)
+
+document.querySelector('span').textContent = 'Realizar curso de JavaScript'
+*/
